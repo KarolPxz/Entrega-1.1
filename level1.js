599c5d0a371be1071772a0fe37c35c55ed646533
Karol Pelaez <qfluis@gmail.com>
2022-06-22 12:52:29 +0200

    exercise 1 level one and exercise 1 level two

---
+++ level1.js
@@ -0,0 +1,10 @@
+//Exercise 1
+//Displays the username and surname of the user using literal templates, saving them in variables and referencing them in the printing of the message.
+
+function seeName(name) {
+
+    console.log(name);
+
+}
+
+seeName('Maria');
---
+++ level2.js
@@ -0,0 +1,10 @@
+// Exercise 2
+//Displays the username and surname of the user using literal templates, saving them in variables and referencing them in the printing of the message.
+
+function seeName(name, surname) {
+
+    console.log(`The user name is: ${name}; and the surname is: ${surname}`);
+
+}
+
+seeName('Maria', 'Pelaez');
