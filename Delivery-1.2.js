dca86cc87da4513411c6a3c0baaa90104ea23891
KarolPxz <karolpelxz@gmail.com>
2022-06-30 16:33:43 +0200

    Added excercise leve 3

--- Delivery-1.2.js
+++ Delivery-1.2.js
@@ -42,3 +42,31 @@ person1.name();
 //Level 3
 //- Exercise 1
 //Write an object creator function that instantiates an abstract class. Label it with different definitions.
+class Human {
+  constructor () {
+    if (this.constructor = Human) {
+      throw Error('This is an abstract class, Human cannot be created');
+    }
+  }
+}
+try {
+  let human = new Human();
+   console.log(human);
+} catch (error) {
+  console.error(error);
+}
+
+
+const Men = function (name) {
+  this.name = name;
+  console.log('this is an instance of an abstract class');
+}
+
+Men.prototype = Object.create(Human.prototype);
+let men = new Men('Toni');
+console.log(men);
+console.log(men instanceof Men);
+console.log(men instanceof Human );
+
+
+
