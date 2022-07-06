6a60f53f8ca15067dc08ea5430d552ed5dc5ee2c
KarolPxz <karolpelxz@gmail.com>
2022-07-06 10:30:49 +0200

    Aded exercise 1, level 1,  delivery 1.3

--- Delivery-1.3.js
+++ Delivery-1.3.js
@@ -1,27 +1,46 @@
 //Level 1
-//- Exercises 1
+//- Exercise 1
 //Creates a function that returns a Promise that invokes the resolve () or reject () function that it receives. Invoke it by passing the two functions to it so that they print a different message depending on whether the Promise is resolved or not.
-let greeting = name => console.log(`Hello ${name}!`);
-const hasMeeting = false;
-const meeting = new Promise((resolve, reject) => {
-if (!hasMeeting) {
-    const meetingDetails = {
-        name: 'Marketing Meeting',
-        location: 'Skype',
-        time: '1:00 PM'
-    }
-    resolve(meetingDetails);
-} else {
-    reject(new Error('Meeting already scheduled'))
-}
-});
 
-meeting
-    .then( res => {
-        //resolve data
-        console.log('Meeting Scheduled');
-        console.log(res);
-    })
-    .catch( err => {
-        console.log(err.message);
+function eaten(feedTheDOg) {
+    return new Promise((resolve, reyect) => {
+        if (!feedTheDOg) {
+            const eatenDetails = {
+                name: 'Breakfast',
+                ingredients: ' Chicken and apple ',
+                time: ' at 8:00 AM '
+            }
+            resolve(`To eat ${ Object.values( eatenDetails ) }, yum yum.`);
+        } else {
+            reyect('The dog had already eaten')
+        }
     });
+}
+
+eaten(false)
+    .then(resp => console.log(resp));
+eaten(true)
+    .catch(error => console.error(error));
+
+//- Exercise 2
+//Creates an arrow function that receives a parameter and a callback function and passes a message or another to the function (which will be printed by console) depending on the parameter received.
+
+
+/*
+function returnPromise() {
+    return new Promise((resolve, reyect) => {
+        setTimeout(() => {
+            let allOk = true;
+            if (allOk) {
+                resolve('All be Ok');
+            } else {
+                reyect('Somenting has gone wrong')
+            }
+        }, 2000)
+    })
+}
+returnPromise()
+    .then(resp => console.log(resp))
+    .catch(error => console.error(error));
+
+*/
