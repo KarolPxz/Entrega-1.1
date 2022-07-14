a14719835757c1245c42088ed42df8dc2de0e5c5
KarolPxz <karolpelxz@gmail.com>
2022-07-14 13:01:18 +0200

    working with tests

--- Release1.6/__tests__/app.test.js
+++ Release1.6/__tests__/app.test.js
@@ -3,27 +3,45 @@ const operations = require('../app/app');
 
 
 test('add two or more numbers', () => {
-   const result = operations.add( 1, 2 ); 
+   const result = operations.add( [ 1, 2 ] ); 
    expect(result).toBe(3);
 
 });
 describe( 'when adding two or more numbers', () => {
    const result = operations.add; 
+   test(' if I receive two or more parameters ', () => {
+      expect(result( [ 1, 2, 3, 9 ] )).toBe(15); 
+   });
    test(' if I receive a single parameter ', () => {
-      expect(result( 1, 5 )).toBe(6); 
+      expect(result( [ 1 ] )).toBe(1); 
    });
-   test(' if I receive a null parameter ', () => {
-      expect(result( 0, 0 )).toBe(0); 
+   test(' if I receive cero parameters ', () => {
+      expect(result( [] )).toBe(0); 
    });
+   test(' if I receive nan parameter ', () => {
+      expect(result( [NaN] )).toBe(NaN); 
+   });
+   test(' if I receive Null parameter ', () => {
+      expect(result( [null] )).toBe(0); 
+   });
+
 });
 
 describe(' when subtract two or more numbers ', () => {
    const result = operations.subtract;
-   test( ' if recibe a single parameter ', () => {
-      expect(result( 0, NaN )).toBeNull();
+   test(' if I receive two or more parameters ', () => {
+      expect(result( [ 1, 2, 3, 9 ] )).toBe(-15); 
    });
-
-   test( ' if recibe Null parameters', () => {
-      expect(result( null, null )).toBeNull();
+   test(' if I receive a single parameter ', () => {
+      expect(result( [ 2 ] )).toBe(2); 
+   });
+   test(' if I receive cero parameters ', () => {
+      expect(result( [] )).toBe(0); 
+   });
+   test(' if I receive nan parameter ', () => {
+      expect(result( [NaN] )).toBe(NaN); 
+   });
+   test(' if I receive Null parameter ', () => {
+      expect(result( [null] )).toBe(0); 
    });
 })
--- Release1.6/app/app.js
+++ Release1.6/app/app.js
@@ -1,10 +1,17 @@
 //Level 1
 //Creates a file with the functions add, subtract, multiply, and divide two or more operands. Test the correct execution of these functions.
 
-let add = ( a, b )=> a + b;
-let subtract = ( a, b )=> a - b;
-let multiply = ( a, b )=> a * b;
-let divide = ( a, b )=> a / b;
+let num = [1, 2, 3, 4, 5];
+let add = ( num )=> num.reduce((a, b) => a + b, 0);
+
+let subtract = ( num )=> num.reduce((a, b) => a - b, 0);
+let multiply = ( num )=> num.reduce((a, b) => a * b, 0);
+let divide = ( num )=> num.reduce((a, b) => a / b, 0);
+
+
+
+
+console.log(add(num));
 //Create the corresponding tests to verify the operation of the two functions of the exercise Promises and Callbacks N1 E2.
 //Create the corresponding tests to verify the operation of the functions of the exercise Promises and Callbacks N2 E1 and Promises and Callbacks N2 E2 (getEmployee() and getSalary()).
 //Create the corresponding tests to verify the operation of the Async/Await N1 E2 exercise.
