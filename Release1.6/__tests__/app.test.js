3f4ff4362354b22db3942caf9dedb9795e087ef7
KarolPxz <karolpelxz@gmail.com>
2022-07-14 12:13:03 +0200

    first trial tests

--- Release1.6/__tests__/app.test.js
+++ Release1.6/__tests__/app.test.js
@@ -4,5 +4,26 @@ const operations = require('../app/app');
 
 test('add two or more numbers', () => {
    const result = operations.add( 1, 2 ); 
-   expect(result).toBe(4);
+   expect(result).toBe(3);
+
 });
+describe( 'when adding two or more numbers', () => {
+   const result = operations.add; 
+   test(' if I receive a single parameter ', () => {
+      expect(result( 1, 5 )).toBe(6); 
+   });
+   test(' if I receive a null parameter ', () => {
+      expect(result( 0, 0 )).toBe(0); 
+   });
+});
+
+describe(' when subtract two or more numbers ', () => {
+   const result = operations.subtract;
+   test( ' if recibe a single parameter ', () => {
+      expect(result( 0, NaN )).toBeNull();
+   });
+
+   test( ' if recibe Null parameters', () => {
+      expect(result( null, null )).toBeNull();
+   });
+})
